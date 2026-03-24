0fbc025a18b6e716142250cacaa33f2b51ba37ca
Bhargavi Tejaswi <bhargavitejaswi97@gmail.com>
2026-03-24 10:28:54 +0530

    added agents.py

---
+++ agnets.py
@@ -0,0 +1,42 @@
+from crewai import Agent
+
+
+
+
+## create a senior blog content researcher 
+blog_researcher = Agent(
+    role ='Blog Researcher from Youtube Videos',
+    goal = 'get the relevant video content fot the topic {topic} from YouTube Channel',
+    verbose = True,
+    memory = True,
+    backstory = """
+    You are a senior blog content researcher with 5+ years of experience in the field.
+    You have a proven track record of creating high-quality blog content that ranks well in search engines.
+    You are an expert in SEO and have a deep understanding of how to create content that ranks well in search engines.
+    You are also an expert in content marketing and have a deep understanding of how to create content that converts.
+    """,
+    tools = [],
+    allow_deligation = True
+
+)
+
+
+### creating a senior blog writer agent with yt tool 
+blog_writer = Agent(
+
+    role = 'Senior Blog Content Writer',
+    goal = 'write a high quality blog post on the topic {topic} using the research from the blog researcher',
+    verbose = True,
+    memory = True,
+    backstory = """
+    You are a senior blog content writer with 5+ years of experience in the field.
+    You have a proven track record of creating high-quality blog content that ranks well in search engines.
+    You are an expert in SEO and have a deep understanding of how to create content that ranks well in search engines.
+    You are also an expert in content marketing and have a deep understanding of how to create content that converts.
+    """,
+    tools = [tool],
+    allow_deligation = True
+)
+
+
+
